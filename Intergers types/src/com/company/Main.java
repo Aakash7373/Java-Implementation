cb1b8d64c03f9a74cf402cc5f7177913121db83b
Akash Pawar <38106864+Aakash7373@users.noreply.github.com>
2018-06-15 20:27:44 -0500

    commit

---
+++ Intergers types/src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	double x=20.3658;
+	double y=152.369;
+	double z=x+y;
+        System.out.println("Value of z is:" + z);
+
+        int a=50;
+        int b=90;
+        boolean p=false;
+
+        if(a>b)
+        {
+            p=true;
+            System.out.println(p);
+        }
+        else
+        {
+            p=false;
+            System.out.println(p);
+        }
+
+        String my = "Akash";
+        String str = "Pawar";
+        String ff = "20.33";
+        System.out.println("New string is "+ my + str + ff);
+
+    }
+}
+
